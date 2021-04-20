bc63955234c91f10023e16e80a34f3fe605bf20c
XxRedPenguinxX <79510002+MarcCucalon@users.noreply.github.com>
2021-04-20 10:09:10 +0200

    a

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -6,9 +6,10 @@ export var speed = Vector2(300,1000)
 var velocitat = Vector2.ZERO
 var moviment = Vector2(0,0)
 var Monedes = 0
+var atacant = false
 func _physics_process(delta):
 	velocitat.x = 0
-	if Input.is_action_just_pressed("ui_up"):
+	if Input.is_action_pressed("ui_up"):
 		if is_on_floor():
 			velocitat.y += -1500
 	if Input.is_action_pressed("ui_left"):
@@ -17,23 +18,29 @@ func _physics_process(delta):
 		velocitat.x = 1200
 	velocitat.y += gravity * delta
 	velocitat = move_and_slide(velocitat,Vector2.UP)
-	if velocitat.x > 0:
-		$AnimatedSprite.flip_h = false
-	elif velocitat.x < 0:
-		$AnimatedSprite.flip_h = true
-	if velocitat.y < 0:
-		$AnimatedSprite.play("Saltar")
-	if velocitat.y > 0:
-		$AnimatedSprite.play("Caure")
-	else:
+	if not atacant:
 		if velocitat.x > 0:
-			$AnimatedSprite.play("Correr")
+			$AnimatedSprite.flip_h = false
 		elif velocitat.x < 0:
-			$AnimatedSprite.play("Correr")
-		if velocitat.x == 0:
-			$AnimatedSprite.play("Quiet")
-	if Input.is_action_just_pressed("Atacar"):
-		$AnimatedSprite.play("Atacar")			
+			$AnimatedSprite.flip_h = true
+		if velocitat.y < 0:
+			$AnimatedSprite.play("Saltar")
+		if velocitat.y > 0:
+			$AnimatedSprite.play("Caure")
+		else:
+			if velocitat.x > 0:
+				$AnimatedSprite.play("Correr")
+			elif velocitat.x < 0:
+				$AnimatedSprite.play("Correr")
+			if velocitat.x == 0:
+				$AnimatedSprite.play("Quiet")
+
+		if Input.is_action_just_pressed("Atacar"):
+			$AnimatedSprite.play("Atacar")		
+			atacant = true
+			
 func _on_AnimatedSprite_animation_finished():
 	if $AnimatedSprite.animation == "Saltar":
 		$AnimatedSprite.animation = "Correr"
+	if $AnimatedSprite.animation == "Atacar":
+		atacant = false
