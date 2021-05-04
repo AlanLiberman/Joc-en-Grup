88ed2a158cd45c189f1724133cacb52e22ee6e40
AlanLiberman <alanliberman11@gmail.com>
2021-05-04 12:27:50 +0200

    un altre vegada organitzant els fitxers

--- Menu.gd
+++ Menu.gd
@@ -2,3 +2,18 @@ extends Sprite
 
 func _ready():
 	region_filter_clip = true
+
+func _on_Level_1_body_entered(body):
+	get_tree().change_scene("res://escenari 1.tscn")
+
+func _on_Level_2_body_entered(body):
+	get_tree().change_scene("res://Nivell 2.tscn")
+
+func _on_Level_3_body_entered(body):
+	get_tree().change_scene("res://Nivell 2.tscn")
+
+func _on_Level_4_body_entered(body):
+	get_tree().change_scene("res://Nivell 2.tscn")
+
+func _on_Level_5_body_entered(body):
+	get_tree().change_scene("res://Nivell 2.tscn")
